0733606c28f21ab41b256e01a43a942d1cfcec13
ADV <advtechlabsglobal@gmail.com>
2025-12-13 11:36:11 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,8 @@ window.SUPABASE_CONFIG = {
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhibGloeWxjcGVwa3VyaWF0cXRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIxMDEwNDIsImV4cCI6MjA3NzY3NzA0Mn0.Ka_dkjTcPmzfhTBmCrUNrfxU_G8UncebUwM5h-Wlhns'
 };
 
+// Initialize only if Supabase library is loaded
 if (window.supabase) {
     window.supabaseClient = window.supabase.createClient(window.SUPABASE_CONFIG.url, window.SUPABASE_CONFIG.anonKey);
-    console.log("Supabase Client Initialized in Config");
+    console.log("Supabase Client Initialized via Config");
 }
